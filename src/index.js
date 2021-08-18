9974f137bcf50a9bbd624dbf0589f92cc15c6a0e
Samuel <Samuelpmontague@gmail.com>
2021-08-18 17:20:18 -0500

    added a calendar component to reservation.js, continuing progress on reservation.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from'react-dom';
+import Calendar from 'react-calendar';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import 'react-calendar/dist/Calendar.css'; 
+
+// const ReactCalendar = () => {
+//   const [date, setDate] = useState(new Date())
+
+//   const onChange = date => {
+//       setDate(date)
+//   }
+//   return (
+//       <div>
+//       <Calendar onChange={onChange} value={date} />
+//       {console.log(date)}
+//       {date.toString()}
+//   </div>
+//   );
+// }
 
 ReactDOM.render(
   <React.StrictMode>
