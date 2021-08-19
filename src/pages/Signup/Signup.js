30382d7d2c0ac6121124178891bfed3674cda250
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-19 12:05:59 -0500

    Merge

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Alert } from 'reactstrap';
-
+import { Form, Button, Alert } from 'react-bootstrap';
 import { createUser } from '../../utils/API';
 import Auth from '../../utils/auth';
 
