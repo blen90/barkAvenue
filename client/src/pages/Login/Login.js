50df738410c020263d7448dfd1ce2e967d663de1
YueshiaVue <cyvue22@gmail.com>
2021-08-19 12:27:41 -0500

    Merge testing

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -6,7 +6,7 @@ import './Login.css';
 const Login = (props) => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  // const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputChange = (e) => {
     // Getting the value and name of the input
@@ -61,12 +61,10 @@ const Login = (props) => {
         </FormGroup>
       
         <Button className="sub"> Submit </Button>
-        <p>I don't have account yet. <a href=""> Register Now </a></p> 
+        <p>I don't have account yet. <a href="/signup"> Register Now </a></p> 
       </div>
     </Form>
   );
 };
 
 export default Login;
-
-
