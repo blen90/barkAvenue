b3eb5e778969d319651669f2d73aef136cbe424a
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-19 09:35:33 -0500

    Added layout of signup

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,46 +1,122 @@
-const Validator = require("validator");
-const isEmpty = require("is-empty");
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import './Signup.css';
 
-module.exports = function validateRegisterInput(data) {
-	let error = {};
+const Signup = (props) => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [petName, setPetName] = useState("");
+  const [petBreed, setPetBreed] = useState("");
 
-	// Convert empty fields to an empty string so we can use validator functions
-	data.name = !isEmpty(data.name) ? data.name : "";
-	data.email = !isEmpty(data.email) ? data.email : "";
-	data.password = !isEmpty(data.password) ? data.password : "";
-	data.passwordValid = !isEmpty(data.passwordValid) ? data.passwordValid : "";
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+    //Set the state to username and password based on the input type,
+    if (inputType === "userName") {
+      setUserName(inputValue);
+    } else {
+      setPassword(inputValue);
+    } 
+  };
 
-	// Checks name if valid
-	if (Validator.isEmpty(data.name)) {
-		error.name = "Name field is required";
-	}
+  return (
+    <Form className="contact">
+      <div>
+        <h3> Signup below:</h3>
 
-	// Checks email if valid
-	if (Validator.isEmpty(data.email)) {
-		error.email = "Email field is required";
-	} else if (!Validator.isEmail(data.email)) {
-		error.email = "Email is invalid";
-	}
+        <FormGroup className="label">
+          <Label for="name"> Username: </Label>
+          <Input
+            type="text"
+            placeholder=" "
+            value={userName}
+            onChange={handleInputChange}
+            name="userName"
+            required
+          />
+        </FormGroup>
 
-	// Checks password if valid
-	if (Validator.isEmpty(data.password)) {
-		error.password = "Password field is required";
-	}
+        <FormGroup>
+          <Label for="password"> Password: </Label>
+          <Input
+            type="password"
+            name="password"
+            placeholder="********"
+            value={password}
+            onChange={handleInputChange}
+            required
+          />
+        </FormGroup>
 
-	if (Validator.isEmpty(data.passwordValid)) {
-		error.passwordValid = "Confirm password field is required";
-	}
+		<FormGroup>
+          <Label for="firstName"> First Name: </Label>
+          <Input
+            type="text"
+            name="firstName"
+            placeholder=""
+            value={firstName}
+            onChange={handleInputChange}
+            required
+          />
+        </FormGroup>
 
-	if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-		error.password = "Password must be at least 6 characters";
-	}
+		<FormGroup>
+          <Label for="lastName"> Last Name: </Label>
+          <Input
+            type="text"
+            name="lastName"
+            placeholder=""
+            value={lastName}
+            onChange={handleInputChange}
+            required
+          />
+        </FormGroup>
 
-	if (!Validator.equals(data.password, data.passwordValid)) {
-		error.passwordValid = "Passwords must match";
-	}
+		<FormGroup>
+          <Label for="phoneNumber"> Phone Number: </Label>
+          <Input
+            type="text"
+            name="phoneNumber"
+            placeholder=""
+            value={phoneNumber}
+            onChange={handleInputChange}
+            required
+          />
+        </FormGroup>
 
-	return {
-		error,
-		isValid: isEmpty(error)
-	};
+		<FormGroup>
+          <Label for="petName"> Pet Name: </Label>
+          <Input
+            type="text"
+            name="petName"
+            placeholder=""
+            value={petName}
+            onChange={handleInputChange}
+            required
+          />
+        </FormGroup>
+
+		<FormGroup>
+          <Label for="petBreed"> Pet Breed: </Label>
+          <Input
+            type="text"
+            name="petBreed"
+            placeholder=""
+            value={petBreed}
+            onChange={handleInputChange}
+            required
+          />
+        </FormGroup>	
+      
+        <Button className="sub"> Submit </Button>
+      </div>
+    </Form>
+  );
 };
+
+export default Signup;
