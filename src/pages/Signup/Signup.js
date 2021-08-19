5407c13ef603fb3bd2948e1a34c1b3fede38464f
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-19 10:58:22 -0500

    Updated signup form

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,122 +1,61 @@
-import React, { useState } from "react";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-import './Signup.css';
+import React from 'react';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const Signup = (props) => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [petName, setPetName] = useState("");
-  const [petBreed, setPetBreed] = useState("");
-
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-    //Set the state to username and password based on the input type,
-    if (inputType === "userName") {
-      setUserName(inputValue);
-    } else {
-      setPassword(inputValue);
-    } 
-  };
-
   return (
-    <Form className="contact">
-      <div>
-        <h3> Signup below:</h3>
-
-        <FormGroup className="label">
-          <Label for="name"> Username: </Label>
-          <Input
-            type="text"
-            placeholder=" "
-            value={userName}
-            onChange={handleInputChange}
-            name="userName"
-            required
-          />
-        </FormGroup>
-
-        <FormGroup>
-          <Label for="password"> Password: </Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="********"
-            value={password}
-            onChange={handleInputChange}
-            required
-          />
-        </FormGroup>
-
-		<FormGroup>
-          <Label for="firstName"> First Name: </Label>
-          <Input
-            type="text"
-            name="firstName"
-            placeholder=""
-            value={firstName}
-            onChange={handleInputChange}
-            required
-          />
-        </FormGroup>
-
-		<FormGroup>
-          <Label for="lastName"> Last Name: </Label>
-          <Input
-            type="text"
-            name="lastName"
-            placeholder=""
-            value={lastName}
-            onChange={handleInputChange}
-            required
-          />
-        </FormGroup>
-
-		<FormGroup>
-          <Label for="phoneNumber"> Phone Number: </Label>
-          <Input
-            type="text"
-            name="phoneNumber"
-            placeholder=""
-            value={phoneNumber}
-            onChange={handleInputChange}
-            required
-          />
-        </FormGroup>
-
-		<FormGroup>
-          <Label for="petName"> Pet Name: </Label>
-          <Input
-            type="text"
-            name="petName"
-            placeholder=""
-            value={petName}
-            onChange={handleInputChange}
-            required
-          />
-        </FormGroup>
-
-		<FormGroup>
-          <Label for="petBreed"> Pet Breed: </Label>
-          <Input
-            type="text"
-            name="petBreed"
-            placeholder=""
-            value={petBreed}
-            onChange={handleInputChange}
-            required
-          />
-        </FormGroup>	
-      
-        <Button className="sub"> Submit </Button>
-      </div>
+    <Form>
+      <FormGroup row>
+        <Label for="exampleEmail" sm={2}>Email</Label>
+        <Col sm={10}>
+          <Input type="email" name="email" id="exampleEmail" placeholder="" />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="examplePassword" sm={2}>Password</Label>
+        <Col sm={10}>
+          <Input type="password" name="password" id="examplePassword" placeholder="******" />
+        </Col>
+      </FormGroup>
+	  <FormGroup row>
+        <Label for="userName" sm={2}>Username</Label>
+        <Col sm={10}>
+          <Input type="userName" name="userName" id="userName" placeholder="" />
+        </Col>
+      </FormGroup>
+	  <FormGroup row>
+        <Label for="petName" sm={2}>Pet Name</Label>
+        <Col sm={10}>
+          <Input type="petName" name="petName" id="petName" placeholder="" />
+        </Col>
+      </FormGroup>
+	  <FormGroup row>
+        <Label for="petBreed" sm={2}>Pet Breed</Label>
+        <Col sm={10}>
+          <Input type="petBreed" name="petBreed" id="petBreed" placeholder="" />
+        </Col>
+      </FormGroup>
+      <FormGroup tag="fieldset" row>
+        <legend className="col-form-label col-sm-2">Gender of pet:</legend>
+        <Col sm={10}>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="radio2" />{' '}
+              Male
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="radio2" />{' '}
+              Female
+            </Label>
+          </FormGroup>
+        </Col>
+      </FormGroup>
+      <FormGroup check row>
+	  	<Button className="sub"> Submit </Button>
+      </FormGroup>
     </Form>
   );
-};
+}
 
 export default Signup;
