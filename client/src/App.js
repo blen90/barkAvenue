b033f26c101f1f0cec417f2416c656fb70dfa1b8
Samuel <Samuelpmontague@gmail.com>
2021-08-20 18:37:41 -0500

    worked on bugs

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 
 
 import About from './pages/About/About';
-import Cart from './pages/Cart/Cart';
 import Checkout from './pages/Checkout/Checkout';
 import Login from './pages/Login/Login.js';
 import Profile from './pages/Profile/Profile';
@@ -43,7 +42,6 @@ class App extends Component {
     console.log(this.state.from)
     return (
       <Router>
-
         <Provider store={store}>
           <div className="flex-column justify-flex-start min-100-vh">
             <Header />
@@ -64,25 +62,6 @@ class App extends Component {
               </Switch>
             </div>
             <Footer />
-
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <Nav />
-          <div className="container">
-             <Switch>
-              {/* <Route exact path="/" component={About} /> */}
-              <Route exact path="/" component={About}/>
-              <Route exact path="/Services" component={Services}/>
-              <Route exact path="/About" component={About}/>
-              <Route exact path="/Checkout" component={Checkout}/>
-              <Route exact path="/Profile" component={Profile}/>
-              <Route exact path="/Reservation" render={props => <Reservation reservationInput = {this.reservationInput}/>}/>
-              <Route exact path= "/Cart" render={props => <Cart to={this.state.to} from={this.state.from}/>} />
-              <Route exact path="/Contact" component={Contact}/>
-              <Route exact path="/Login" component={Login}/>
-              <Route exact path="/Signup" component={Signup}/> 
-            </Switch>
-
           </div>
         </Provider>
       </Router>
