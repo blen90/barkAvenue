318c886dd713dfd77c74b753b41fdd19ea72b55f
YueshiaVue <cyvue22@gmail.com>
2021-08-20 15:00:43 -0500

    added cart reducer and actions

--- client/src/reducers/cart.js
+++ client/src/reducers/cart.js
@@ -1,6 +1,21 @@
-import { combineReducers } from 'redux';
-import cart from './cart';
+import {ADD_TO_CART} from './actions/cart';
 
-export default combineReducers({
-    cart,
-})
+const initialState = {
+    items: [
+        // example data
+        // { userid:1, package: 'basic', dateFrom: '8/1/21', dateTo: '8/2/21' }
+    ]
+}
+
+const cart = (state = initialState,action)  => {
+    switch(action.type) {
+        case ADD_TO_CART:
+            let item = action.data;
+            let items = state.items;
+            return {state,items: [...items,item]}
+        default:
+            break;
+    }
+}
+
+export default cart;
