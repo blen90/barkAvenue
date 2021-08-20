fe9208aa5a08ae7205d2b84e9c6c8a2938bd6b0f
Blenda Orellana <blen.or90@gmail.com>
2021-08-20 09:20:06 -0500

    worked on adding cards to reservation

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,11 @@ import About from './pages/About/About';
 import Checkout from './pages/Checkout/Checkout';
 import Login from './pages/Login/Login.js';
 import Profile from './pages/Profile/Profile';
-import Services from './pages/Services/Services.js';
+// import Services from './pages/Services/Services.js';
 import Signup from './pages/Signup/Signup.js';
 import Contact from './pages/Contact/Contact.js';
 import Reservation from './pages/Reservation/Reservation.js'
-import Cart from './pages/Cart/Cart.js'
+// import Cart from './pages/Cart/Cart.js'
 
 import Footer from './pages/components/Footer';
 import Header from './pages/components/Header.js';
@@ -45,12 +45,12 @@ class App extends Component {
              <Switch>
               {/* <Route exact path="/" component={About} /> */}
               <Route exact path="/" component={About} />
-              <Route exact path="/Services" component={Services} />
+              {/* <Route exact path="/Services" component={Services} /> */}
               <Route exact path="/About" component={About} />
               <Route exact path="/Checkout" component={Checkout} />
               <Route exact path="/Profile" component={Profile} />
               <Route exact path="/Reservation" render={props => <Reservation reservationInput = {this.reservationInput}/>} />
-              <Route exact path= "/Cart" render={props => <Cart to={this.state.to} from={this.state.from}/>} />
+              {/* <Route exact path= "/Cart" render={props => <Cart to={this.state.to} from={this.state.from}/>} /> */}
               <Route exact path="/Contact" component={Contact} />
               <Route exact path="/Login" component={Login} />
               <Route exact path="/Signup" component={Signup} /> 
